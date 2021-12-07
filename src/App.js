d4175197a621cdebdf2df268605980c85ec63e06
Akramjon Mamasoliev <akramjonmamasoliev@Akramjons-MacBook-Pro.local>
2021-12-07 14:52:28 +0900

    file upload web

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+  const imageInputRef = useRef(null)
+  const triggerUpload = () => {
+    imageInputRef.current.click()
+  }
+  const onMessage = (data) => {
+    let params = data?.data;
+
+    if (typeof data?.data === 'string') {
+      params = JSON.parse(params);
+    }
+    if (typeof params !== 'object') return;
+    const { type } = params;
+
+    if(type === 'upload'){
+      window.focus()
+      triggerUpload()
+    }
+
+  };
+
+  useEffect(() => {
+    window.addEventListener('message', onMessage);
+    return () => {
+      window.removeEventListener('message', onMessage);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input 
+       ref={imageInputRef}
+       type="file"
+       id="fileupload"
+       name="avatar"
+       accept="image/png, image/jpeg" 
+    />
+    <div style={{marginTop: 20}}>
+      <textarea id='textarea' name='textarea' rows={4}/>
+    </div>
+    <div id='element-1'></div>
+    <div id='element-2'></div>
+    <div style={{marginTop: 30}}>
+      <button onMouseDown={triggerUpload}>Tiggger Upload inside web</button>
+    </div>
     </div>
   );
 }
